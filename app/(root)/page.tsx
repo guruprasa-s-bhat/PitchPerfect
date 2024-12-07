f72ca22fb9542cba6e1d7e74e0d35cae3d22774e
Guruprasad Bhat <guruprasadbhat106@gmail.com>
2024-12-07 17:34:59 +0530

    done with the startup card UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,28 @@
 import SearchForm from "@/components/SearchForm";
+import Startupcard from "@/components/Startupcard";
 import React from "react";
 
-const Home = async (searchParams: Promise<{ query?: string }>) => {
+const Home = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) => {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Guru" },
+      _id: 1,
+      description: "this is description",
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCGg_GZiPH1hVG2NfTnLS45vs5e47I_VQxkg&s",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
+
   return (
     <>
       <section className="pink_container">
@@ -14,6 +34,20 @@ const Home = async (searchParams: Promise<{ query?: string }>) => {
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
         </p>
         <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid ">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <Startupcard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No Startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
